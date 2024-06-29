2d80d73cd694e9b744923856a477d4eb0a7d0c76
Santhosh Thota <santhosh2011@gmail.com>
2024-06-29 09:03:40 +0530

    Adding Generic Queue implementation
    Generic queue implementation uses linked list to store the enteries

---
+++ dcoding/queue/genericQueue.java
@@ -0,0 +1,40 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+
+
+class GenericQueue<T> {
+    
+    private List<T> list;
+    
+    public Queue(){
+        this.list = new LinkedList<T>();
+    }
+    
+    void enqueue(T t){
+        list.add(t);
+    }
+    
+    T dequeue(){
+        if(list.size()==0){
+            return null;
+        }
+        T current = list.get(0);
+        
+        list.remove(current);
+        return current;
+    }
+    
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<Integer>();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        System.out.println(1==queue.dequeue());
+        System.out.println(2==queue.dequeue());
+        queue.enqueue(4);
+        System.out.println(3==queue.dequeue());
+        System.out.println(4==queue.dequeue());
+         System.out.println(null==queue.dequeue());
+    }
+}
