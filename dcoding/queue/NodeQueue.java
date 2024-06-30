7445634fffbd7e98c8f310ed4c373f5770468f6d
Santhosh Thota <santhosh2011@gmail.com>
2024-06-30 13:28:09 +0530

    Update and rename nodeQueue.java to NodeQueue.java

--- dcoding/queue/NodeQueue.java
+++ dcoding/queue/NodeQueue.java
@@ -19,7 +19,7 @@ class NodeQueue<T> {
     
     private int size;
     
-    public Queue(){
+    public NodeQueue(){
         this.size = 0;
     }
     
@@ -46,7 +46,7 @@ class NodeQueue<T> {
     }
     
     public static void main(String[] args) {
-        Queue<Integer> queue = new Queue<Integer>();
+        NodeQueue<Integer> queue = new NodeQueue<Integer>();
         queue.enqueue(1);
         queue.enqueue(2);
         queue.enqueue(3);
